1b7eb06221c9d137f520d5ce2b5f2fc646dc59ed
Dragos <dragosct10@gmail.com>
2018-10-16 12:55:55 +0300

    changed footer home button link

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -33,7 +33,7 @@
 export default {
   data: () => ({
     links: [
-      { name: 'Home', Link: '/#' },
+      { name: 'Home', Link: '/dashboard' },
       { name: 'Creative Tim', Link: 'https://www.creative-tim.com' },
       { name: 'About Us', Link: 'https://creative-tim.com/presentation' },
       { name: 'Blog', Link: 'https://blog.creative-tim.com' }
