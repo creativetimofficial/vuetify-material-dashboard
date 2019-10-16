3ddf53d951072893be75a41e4411161cb64363b3
Dragos <dragosct10@gmail.com>
2019-10-16 17:15:07 +0300

    feat: add copyrights

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+// =========================================================
+// * Vuetify Material Dashboard - v1.0.0
+// =========================================================
+
+// * Product Page: https://www.creative-tim.com/product/vuetify-material-dashboard
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+// * Licenses under MIT
+
+// * Coded by Creative Tim
+
+// =========================================================
+
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
